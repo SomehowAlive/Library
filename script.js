0d0d42e3e4078800d182fda6aa2b898e7f96f727
somehowalive <slow.optimism@gmail.com>
2023-02-02 23:12:52 +0100

    added functions to create a book card from a book object and to display our array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const booksList = document.querySelector(".books-list");
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -9,4 +11,23 @@ function Book(title, author, pages, isRead) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function createBookElement(book, index) {
+  const bookElem = document.createElement(`
+  <li class="book ${book.isRead ? "read" : ""} data-id="${index}">
+    <p class="book-title">${book.title}</p>
+    <p class="book-author">${book.author}</p>
+    <p class="book-pages">${book.pages}</p>
+    <button type="button" class="btn view-button">view</button>
+    <button type="button" class="btn delete-button">delete</button>
+  </li>`);
+  return bookElem;
+}
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary; i++) {
+    const bookElem = createBookElement(myLibrary[i], i);
+    booksList.append(bookElem);
+  }
 }
